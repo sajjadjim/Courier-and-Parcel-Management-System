01e6e785f10a5ab03303f5aa06648cda421bf5b8
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-12-12 16:52:14 +0600

    file path destination error  solve

--- src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Loading from '../../../components/Loading';
+import Loading from '../../../Components/Loading';
 import UserDashboard from './UserDashboard';
 import RiderDashboard from './RiderDashboard';
 import AdminDashboard from './AdminDashboard';
