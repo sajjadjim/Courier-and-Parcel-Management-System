8281d050ed3b89b080506f1ee3651e41f90e3da0
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-12-13 19:04:26 +0600

    Environment variale add

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 
-
 // For React Animation AOS 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
